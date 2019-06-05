9f01af811c060616dfad4c822394de6366c131eb
angristan <angristan@pm.me>
2019-06-05 22:45:06 +0200

    Auth: Make POST req to API + get token from JSON

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,18 +1,33 @@
 package sample;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import javafx.fxml.FXML;
-
 import javafx.scene.control.TextField;
+import java.io.IOException;
 
 public class Controller {
     @FXML
-    private TextField usernameField;
+    private TextField emailField;
     @FXML
     private TextField passwordField;
 
     @FXML
     public void onLoginButtonClicked() {
-        System.out.println("username: " + usernameField.getText());
+        System.out.println("email: " + emailField.getText());
         System.out.println("password: " + passwordField.getText());
+
+        try {
+            String tokenJsonString = JavaPostRequest.login(emailField.getText(), passwordField.getText());
+
+            JsonObject tokenJson = new JsonParser().parse(tokenJsonString).getAsJsonObject();
+
+            String token = tokenJson.get("token").getAsString();
+
+            System.out.println(token);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
---
+++ src/sample/JavaPostRequest.java
@@ -0,0 +1,60 @@
+package sample;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class JavaPostRequest {
+
+    private static HttpURLConnection con;
+
+    public static String login(String email, String password) throws IOException {
+
+        String url = "http://localhost:3000/auth";
+        String urlParameters = "email=" + email + "&password=" + password;
+        byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
+
+        try {
+
+            URL myurl = new URL(url);
+            con = (HttpURLConnection) myurl.openConnection();
+
+            con.setDoOutput(true);
+            con.setRequestMethod("POST");
+            con.setRequestProperty("User-Agent", "FFW Menu client");
+            con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+                wr.write(postData);
+            }
+
+            StringBuilder content;
+
+            try (BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()))) {
+
+                String line;
+                content = new StringBuilder();
+
+                while ((line = in.readLine()) != null) {
+                    content.append(line);
+                    content.append(System.lineSeparator());
+                }
+            }
+
+            System.out.println(content.toString());
+
+            return content.toString();
+
+        } finally {
+
+            con.disconnect();
+        }
+    }
+}
