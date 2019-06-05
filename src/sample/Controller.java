3a90c8ee82690eaa49369be37475889c40581a51
angristan <angristan@pm.me>
2019-06-05 22:13:55 +0200

    Get username and password for TextFields + cleanup

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,18 @@
 package sample;
 
+import javafx.fxml.FXML;
+
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    public void onLoginButtonClicked() {
+        System.out.println("username: " + usernameField.getText());
+        System.out.println("password: " + passwordField.getText());
+    }
 }
