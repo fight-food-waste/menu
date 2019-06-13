84bb9eb740da114152c4d6f5f46a4cfdfd482087
angristan <angristan@pm.me>
2019-06-13 15:42:14 +0200

    Get barcodes from products
    J'ai vraiment trop galéré sur ça putain

--- src/ffw_menu/Controller.java
+++ src/ffw_menu/Controller.java
@@ -1,5 +1,6 @@
 package ffw_menu;
 
+import com.google.gson.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,6 +11,7 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Controller {
     @FXML
@@ -44,6 +46,16 @@ public class Controller {
             String products = Product.getFromStock(apiInstance.getToken());
 
             System.out.println(products);
+
+            JsonArray productsJson = new JsonParser().parse(products).getAsJsonArray();
+
+            for (JsonElement product : productsJson) {
+                JsonObject productObj = product.getAsJsonObject();
+                String barcode = productObj.get("barcode").getAsString();
+
+                System.out.println(barcode);
+            }
+
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
